cda2444aa9d4b0c0829817d6246ade0586cc6724
Yubin Kim <yubink@cmu.edu>
2013-10-15 18:22:09 -0400

    Emergency commit

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <algorithm>
+
 #include "indri/QueryEnvironment.hpp"
 #include "indri/Repository.hpp"
 #include "indri/CompressedCollection.hpp"
 
+char* getOption(char ** begin, char ** end, const std::string & option) {
+  char ** itr = std::find(begin, end, option);
+  if (itr != end && ++itr != end) {
+    return *itr;
+  }
+  return 0;
+}
 
-int main(void) {
-	puts("Hello World!!!");
+bool hasOption(char** begin, char** end, const std::string& option) {
+  return std::find(begin, end, option) != end;
+}
 
-	return EXIT_SUCCESS;
+int main(int argc, char * argv[]) {
+
+  if (strcmp(argv[0], "build") == 0) {
+    char* paramFile = getOption(argv, argv + argc, "-p");
+
+    ifstream file;
+    file.open(paramFile);
+
+    string line;
+    if (file.is_open())
+    {
+      while ( getline (file, line) )
+      {
+
+        std::cout << line << endl;
+      }
+      file.close();
+    }
+
+    indri::collection::Repository repo;
+
+
+  } else if (strcmp(argv[0], "run") == 0) {
+
+  } else {
+    std::cout << "Unrecognized option." << std::endl;
+  }
+
+  puts("Hello World!!!");
+
+  return EXIT_SUCCESS;
 }
