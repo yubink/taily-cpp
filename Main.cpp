81c2d120babe430d62d53e86c05b2d587f71bf00
Yubin Kim <yubink@cmu.edu>
2013-10-15 17:03:02 -0400

    Initial commit with Main class

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "indri/QueryEnvironment.hpp"
+#include "indri/Repository.hpp"
+#include "indri/CompressedCollection.hpp"
+
+
+int main(void) {
+	puts("Hello World!!!");
+
+	return EXIT_SUCCESS;
+}
